6514e7785e74a0b97e097335206d9165bca4db02
Mrezzoukc1 <mrezzouk@iut.univ-paris8.fr>
2024-05-28 09:39:53 +0200

    Ennemi + gérer les pv + image pv

--- src/main/java/universite_paris8/iut/abenibrahim/sae_dev2/controleur/Controleur.java
+++ src/main/java/universite_paris8/iut/abenibrahim/sae_dev2/controleur/Controleur.java
@@ -2,27 +2,18 @@ package universite_paris8.iut.abenibrahim.sae_dev2.controleur;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.image.Image;
-import java.awt.*;
-import java.awt.event.KeyListener;
-
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import universite_paris8.iut.abenibrahim.sae_dev2.modele.Environnement;
-import universite_paris8.iut.abenibrahim.sae_dev2.modele.Joueur;
-import universite_paris8.iut.abenibrahim.sae_dev2.modele.Map;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -47,7 +38,6 @@ public class Controleur implements Initializable {
         initAnimation();
         gameLoop.play();
         environnement.getGuts().pvProperty().addListener((obs, oldValue, newValue) -> updatePvJoueurImage());
-
     }
 
     private void initAnimation() {
@@ -73,6 +63,7 @@ public class Controleur implements Initializable {
                             environnement.getEnnemi().setX(newX);
                             environnement.getEnnemi().setY(newY);
                             environnement.getEnnemi().Attaquer();
+                            System.out.println("pv : " + environnement.getGuts().getPv());
                         }
                     }
                     temps++;
@@ -107,7 +98,6 @@ public class Controleur implements Initializable {
                         i1Map.setFitWidth(50);
                         this.tilePaneMap.getChildren().add(i1Map);
                     }
-                }
             }
         }
     }
@@ -116,6 +106,8 @@ public class Controleur implements Initializable {
     public void creerSpriteJoueur(){
         Image g = new Image("file:src/main/resources/universite_paris8/iut/abenibrahim/sae_dev2/2347000-middle-removebg-preview.png");
         ImageView gSprite = new ImageView(g);
+        gSprite.setFitWidth(50);
+        gSprite.setFitHeight(50);
         this.PaneMap.getChildren().add(gSprite);
         ControleurTouche deplacementFleche = new ControleurTouche(this.environnement);
         PaneMap.addEventHandler(KeyEvent.KEY_PRESSED, deplacementFleche);
@@ -130,6 +122,7 @@ public class Controleur implements Initializable {
         a.translateXProperty().bind(environnement.getEnnemi().XProprety());
         a.translateYProperty().bind(environnement.getEnnemi().YProprety());
     }
+
     private void updatePvJoueurImage() {
         ImageView pvImageView = new ImageView();
         this.PaneMap.getChildren().add(pvImageView);
